9c5e5ff989a53555216a1b59c891fbc1172382a7
man0j001 <manojprajapati1208@gmail.com>
2025-11-06 23:01:52 +0530

    Keeping truncated text for previews/citations

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
 export function convertToAscii(inputString:string){
   //remove non ascii character from string
-  const acciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
-  return acciiString;
+  const acciiStrings = inputString.replace(/[^\x00-\x7F]+/g, "");
+  return acciiStrings;
 }
